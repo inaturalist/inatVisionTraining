b11564c96c8a55bb89bd917be4e937c9f9a6d28e
Alex Shepard <alex@meatfreezer.com>
2023-04-08 18:11:12 -0700

    update how we load & make models
    especially note that we don't dropout after final avg pooling anymore

--- nets/nets.py
+++ nets/nets.py
@@ -10,6 +10,7 @@ def make_neural_network(
     n_classes,
     input_dtype,
     train_full_network,
+    ckpt,
 ):
     image_size_with_channels = image_size + [3]
     base_arch = keras.applications.Xception if base_arch_name == "xception" else None
@@ -17,14 +18,19 @@ def make_neural_network(
         print("Unsupported base architecture.")
         return None
 
-    input_layer = keras.layers.Input(shape=image_size_with_channels, dtype=input_dtype)
-    base_model = base_arch(input_tensor=input_layer, weights=weights, include_top=False)
-    base_model.trainable = train_full_network
+    inputs = keras.layers.Input(shape=image_size_with_channels, dtype=input_dtype)
 
-    avg = keras.layers.GlobalAveragePooling2D()(base_model.output)
-    dropout = keras.layers.Dropout(dropout_pct)(avg)
-    x = keras.layers.Dense(n_classes, name="dense_logits")(dropout)
+    base_model = base_arch(
+        input_shape=image_size_with_channels, weights=weights, include_top=False
+    )
+    base_model.trainable = train_full_network
+    if ckpt is not None:
+        base_model.load_weights(ckpt)
+
+    x = base_model(inputs)
+    x = keras.layers.GlobalAveragePooling2D()(x)
+    x = keras.layers.Dense(n_classes, name="dense_logits")(x)
     output = keras.layers.Activation("softmax", dtype="float32", name="predictions")(x)
-    model = keras.Model(inputs=base_model.input, outputs=output)
+    model = keras.Model(inputs=inputs, outputs=output)
 
     return model
